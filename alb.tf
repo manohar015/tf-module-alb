280ae8487a6f4b6fa348ec1cf8f04520bcf7aef0
mvpc-11232 <mvp2612@hotmail.com>
2022-11-03 06:36:22 +0530

    Auto Push Commit

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "alb" {
   internal           = var.INTERNAL
   load_balancer_type = "application"
   security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]  # Conditional expression
-  subnets            = 
+  subnets            = var.INTERNAL ? 
   tags = {
     Environment = "var.ALB_NAME"
   }
