beb135e620e50d1d3b8e06c6f152fc53ecdc3a59
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 07:11:36 +0530

    Auto Push Commit

--- listener.tf
+++ listener.tf
@@ -1,12 +1,18 @@
 resource "aws_lb_listener" "private" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  count             =  var.INTERNAL ? 1 : 0  
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    type             = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Okie"
+      status_code  = "200"
+    }
+
   }
 }
